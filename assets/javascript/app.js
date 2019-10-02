8b81fb985f9511a6582517a1af7952d2cebaa243
stellie82 <kims82@gmail.com>
2019-10-01 18:20:02 -0700

    add ajax call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,47 +1,66 @@
-// Create an array of 90s shows.
-var shows = ["Friends", "Seinfeld"];
+$(document).ready(function () {
 
-function renderButtons() {
+    // Create an array of 90s shows.
+    var shows = ["Friends", "Seinfeld", "Frasier"];
 
-    // Clear the list of shows prior to adding new shows.
-    $("#show-buttons").empty();
+    function displayInfo() {
+        var showName = $(this).attr("data");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?limit=10&rating=PG-13&q=" + tvShow + "&api_key=3vzQRZcFXY3zkusaoBZc5T6RrA4IJcVu";
 
-    for (i = 0; i < shows.length; i++) {
-        var a = $("<button>");
-        a.addClass("show");
-        a.attr("data-name", shows[i]);
-        a.text(shows[i]);
-        $("show-buttons").append(a);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+            // Create a div to hold the movie.
+            var showDiv = $("<div class='show'>");
+
+            // Store and display the rating data.
+            var rating = response.Rated;
+            var ratingText = $("<p>").text("Rating: " + rating);
+            showDiv.append(ratingText);
+
+            // Store and display the image.
+            var imgURL = response.Poster;
+            var image = $("<img>").attr("src", imgURL);
+            showDiv.append(image);
+
+            // Put the show above the previously displayed show.
+            $("#show-view").prepend(showDiv);
+        });
+    }
+
+    function renderButtons() {
+
+        // Clear the list of shows prior to adding new shows.
+        $("#show-buttons").empty();
+
+        for (i = 0; i < shows.length; i++) {
+            var a = $("<button>");
+            a.addClass("show");
+            a.attr("data", shows[i]);
+            a.text(shows[i]);
+            $("#show-buttons").append(a);
+        };
     };
-};
 
-// Create function to trigger AJAX call.
-$("#find-show").on("click", function (event) {
+    // Create function to trigger AJAX call.
+    $("#find-show").on("click", function (event) {
 
-    // Preventing the submit button from trying to submit the form.
-    event.preventDefault();
+        // Preventing the submit button from trying to submit the form.
+        event.preventDefault();
 
-    var tvShow = $("#show-input").val().trim();
+        var show = $("#show-input").val().trim();
 
-    shows.push(tvShow);
+        shows.push(show);
+
+        renderButtons();
+    });
+
+    $(document).on("click", ".show", testAlert);
 
     renderButtons();
+
+
+
 });
-
-document.on("click", ".show", testAlert);
-
-renderButtons();
-
-
-// var queryURL = "https://api.giphy.com/v1/gifs/search?limit=10&rating=PG-13&q=" + tvShow + "&api_key=3vzQRZcFXY3zkusaoBZc5T6RrA4IJcVu";
-
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-// }).then(function (response) {
-//     $("#show-view").text(JSON.stringify(response));
-// });
-
-
-// $(document).ready(function () {
-// });
